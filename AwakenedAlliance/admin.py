c490b7d63eab93ba03bd70e071a358cc8a4381f5
YangQing-Lin <1665486570@qq.com>
2023-02-09 19:52:43 +0800

    给数据库添加了一个Player表

--- AwakenedAlliance/admin.py
+++ AwakenedAlliance/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from AwakenedAlliance.models.player.player import Player
+from AwakenedAlliance.models.player.player import Player
 
 # 将修改的数据库表注册到数据库中
-# admin.site.register(Player)
+admin.site.register(Player)
--- AwakenedAlliance/models/player/player.py
+++ AwakenedAlliance/models/player/player.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # 存储player数据表的信息
-class Player():
+class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.URLField(max_length=256, blank=True)
 
     # 展示数据的字符串
     def __str__(self):
-        return str(user)
+        return str(self.user)
