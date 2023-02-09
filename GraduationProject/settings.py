501240c6df943058f964c219ea1a34de48ab7d66
YangQing-Lin <1665486570@qq.com>
2023-02-09 20:15:22 +0800

    getinfo：获取用户信息

--- GraduationProject/settings.py
+++ GraduationProject/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# 静态文件存放的项目目录位置（game/static)
+# 静态文件存放的项目目录位置（AwakenedAlliance/static)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
