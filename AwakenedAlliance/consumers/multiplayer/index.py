c0a37a77528e1641f34b31173252e04872c1cce7
YangQing-Lin <linqingchuan0826@163.com>
2023-03-20 11:15:43 +0800

    编写thrift服务，成功实现匹配池功能，能够匹配玩家并在后台输出信息，添加了DXC提交代码到服务器的脚本

--- AwakenedAlliance/consumers/multiplayer/index.py
+++ AwakenedAlliance/consumers/multiplayer/index.py
@@ -3,6 +3,14 @@ import json
 from django.conf import settings
 from django.core.cache import cache
 
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+from MatchSystem.src.match_server.match_service import Match
+
+
 class MultiPlayer(AsyncWebsocketConsumer):
     # 前端创建连接的时候调用
     async def connect(self):
@@ -13,56 +21,86 @@ class MultiPlayer(AsyncWebsocketConsumer):
     # 前端刷新或者close()时会执行，但不一定（比如电脑突然断电）
     async def disconnect(self, close_code):
         print('disconnect')
-        await self.channel_layer.group_discard(self.room_name, self.channel_name)
+        if self.room_name:
+            await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
 
     # 创建新的玩家时分组，并群发消息
     async def create_player(self, data):
         self.room_name = None
-        
-        for i in range(1000):
-            name = "room-%d" %(i)
-            if not cache.has_key(name) or len(cache.get(name)) < settings.ROOM_CAPACITY:
-                self.room_name = name
-                break
-        
-        if not self.room_name:
-            print("[MY ERROR] do not have enought room name")
-            return
-        
-        if not cache.has_key(self.room_name):
-            cache.set(self.room_name, [], 3600)  # 对战有效期一个小时
-        
-        # 建立连接之后服务器向本地发送当前所有的玩家信息
-        for player in cache.get(self.room_name):
-            # json.dumps：将一个json数据结构转换成字符串
-            await self.send(text_data=json.dumps({
-                'event': "create_player",
-                'uuid': player['uuid'],
-                'username': player['username'],
-                'photo': player['photo'],
-            }))
+        self.uuid = data['uuid']
+        # Make socket
+        print("服务器调用thrift")
+        transport = TSocket.TSocket('127.0.0.1', 9090)
 
-        await self.channel_layer.group_add(self.room_name, self.channel_name)
+        # Buffering is critical. Raw sockets are very slow
+        transport = TTransport.TBufferedTransport(transport)
 
-        players = cache.get(self.room_name)
-        players.append({
-            'uuid': data['uuid'],
-            'username': data['username'],
-            'photo': data['photo'],
-        })
-        cache.set(self.room_name, players, 3600)
-        # 群发消息
-        await self.channel_layer.group_send(
-            self.room_name,
-            {
-                'type': "group_send_event",
-                'event': "create_player",
-                'uuid': data['uuid'],
-                'username': data['username'],
-                'photo': data['photo'],
-            }
-        )
+        # Wrap in a protocol
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+        # Create a client to use the protocol encoder
+        client = Match.Client(protocol)
+
+        print(data['uuid'], type(data['uuid']))
+
+        print("打开transport")
+        # Connect!
+        transport.open()
+
+        print("准备添加player")
+        client.add_player(1500, data['uuid'], data['username'], data['photo'], self.channel_name)
+        print("添加player成功")
+
+        # Close!
+        transport.close()
+
+        # 之前的代码
+        # self.room_name = None
+        
+        # for i in range(1000):
+        #     name = "room-%d" %(i)
+        #     if not cache.has_key(name) or len(cache.get(name)) < settings.ROOM_CAPACITY:
+        #         self.room_name = name
+        #         break
+        
+        # if not self.room_name:
+        #     print("[MY ERROR] do not have enought room name")
+        #     return
+        
+        # if not cache.has_key(self.room_name):
+        #     cache.set(self.room_name, [], 3600)  # 对战有效期一个小时
+        
+        # # 建立连接之后服务器向本地发送当前所有的玩家信息
+        # for player in cache.get(self.room_name):
+        #     # json.dumps：将一个json数据结构转换成字符串
+        #     await self.send(text_data=json.dumps({
+        #         'event': "create_player",
+        #         'uuid': player['uuid'],
+        #         'username': player['username'],
+        #         'photo': player['photo'],
+        #     }))
+
+        # await self.channel_layer.group_add(self.room_name, self.channel_name)
+
+        # players = cache.get(self.room_name)
+        # players.append({
+        #     'uuid': data['uuid'],
+        #     'username': data['username'],
+        #     'photo': data['photo'],
+        # })
+        # cache.set(self.room_name, players, 3600)
+        # # 群发消息
+        # await self.channel_layer.group_send(
+        #     self.room_name,
+        #     {
+        #         'type': "group_send_event",
+        #         'event': "create_player",
+        #         'uuid': data['uuid'],
+        #         'username': data['username'],
+        #         'photo': data['photo'],
+        #     }
+        # )
 
 
     async def move_toward(self, data):
---
+++ scripts/rsync_AA_to_DXC.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+rsync -rltDvz --progress aa:GraduationProject/ /mnt/c/Users/16654/Desktop/Project/Django/Awakened-Alliance-backend/
