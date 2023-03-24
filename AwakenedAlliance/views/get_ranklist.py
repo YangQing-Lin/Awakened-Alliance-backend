e05136f8aa7df9dd9e387bcc27f24c69f291b2bd
YangQing-Lin <linqingchuan0826@163.com>
2023-03-24 12:49:29 +0800

    修改查看排行榜的函数，能根据传入的参数查询不同模式分数的排行榜

--- AwakenedAlliance/views/get_ranklist.py
+++ AwakenedAlliance/views/get_ranklist.py
@@ -9,6 +9,63 @@ class GetRankListView(APIView):
     permission_classes = ([IsAuthenticated])
 
     def get(self, request):
+        select_type = request.GET.get('select_type', 0)
+        res = {'me': "res test"}
+        if select_type == "rank score":
+            res = self.get_rank_list(request)
+            # me = Player.objects.get(user=request.user)
+            # res = {
+            #     'me': {
+            #         'username': me.user.username,
+            #         'photo': me.photo,
+            #         'score': me.rank_score,
+            #         'rank': Player.objects.filter(rank_score__gt=me.rank_score).count() + 1,
+            #     },
+            #     'all': [],
+            # }
+
+            # players = Player.objects.all().order_by('-rank_score')[:10]
+            # for player in players:
+            #     res['all'].append({
+            #         'username': player.user.username,
+            #         'photo': player.photo,
+            #         'score': player.rank_score,
+            #         'rank': Player.objects.filter(rank_score__gt=player.rank_score).count() + 1,
+            #     })
+
+            # return Response(res)
+        elif select_type == "single mode score":
+            res = self.get_single_mode_list(request)
+
+        return Response(res)
+
+
+    def get_single_mode_list(self, request):
+        # 没有登录可以用user_id=1强制搜索第一个用户，user=request.user就指的是登录的用户
+        me = Player.objects.get(user=request.user)
+        res = {
+            'me': {
+                'username': me.user.username,
+                'photo': me.photo,
+                'score': me.single_mode_score,
+                'rank': Player.objects.filter(single_mode_score__gt=me.single_mode_score).count() + 1,
+            },
+            'all': [],
+        }
+
+        players = Player.objects.all().order_by('-single_mode_score')[:10]
+        for player in players:
+            res['all'].append({
+                'username': player.user.username,
+                'photo': player.photo,
+                'score': player.single_mode_score,
+                'rank': Player.objects.filter(single_mode_score__gt=player.single_mode_score).count() + 1,
+            })
+
+        return res
+    
+
+    def get_rank_list(self, request):
         # 没有登录可以用user_id=1强制搜索第一个用户，user=request.user就指的是登录的用户
         me = Player.objects.get(user=request.user)
         res = {
@@ -30,4 +87,4 @@ class GetRankListView(APIView):
                 'rank': Player.objects.filter(rank_score__gt=player.rank_score).count() + 1,
             })
 
-        return Response(res)
+        return res
